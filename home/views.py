3486c78f4fc33ec0c922f7ac765c29b12920a554
lukesj07 <lukesjennings07@gmail.com>
2025-01-26 17:20:40 -0500

    add bootstrap and more features to base pages

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "home/index.html")
+    template_data = {}
+    template_data["title"] = "GT Movie Store"
+    return render(request, "home/index.html", {
+        "template_data": template_data
+    })
 
 def about(request):
-    return render(request, "home/about.html")
+    template_data = {}
+    template_data["title"] = "About"
+    return render(request, "home/about.html", {
+        "template_data": template_data
+    })
