c886df6dffbe5ede1acbf86b03070b3d811f4098
Daniel Dixon <ddgt5392c@gmail.com>
2025-01-28 23:39:18 -0500

    Added custom error list/rendering for signup form.

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import redirect, render
-
-# Create your views here.
-from django.shortcuts import render
+from .forms import CustomUserCreationForm, CustomErrorList
 from django.http import HttpRequest
-from django.contrib.auth.forms import UserCreationForm
+
 
 def signup(request: HttpRequest) -> render: 
     template_data = {}
@@ -11,12 +9,12 @@ def signup(request: HttpRequest) -> render:
     
     # provide the view of the form
     if request.method == 'GET':
-        template_data['form'] = UserCreationForm()
+        template_data['form'] = CustomUserCreationForm()
         return render(request, 'accounts/signup.html',
                       {'template_data': template_data})
     elif request.method == 'POST':
         # if submitting the form, then we save the user into the database and redirect to the home page
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
             return redirect('home.index')
