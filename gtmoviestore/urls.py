9d610ed66867a17bd1e3f516294dbd2aa8667b03
Daniel Dixon <100236593+DanielDixon32@users.noreply.github.com>
2025-01-30 17:04:10 -0500

    Feature/user (#7)
    * added accounts
    
    * Created accounts app, updated settings and added reference in base.html to signup
    
    * Added custom error list/rendering for signup form.
    
    * Finished signup functionality and created working login page.
    
    * Fixed typo in base.html

--- gtmoviestore/urls.py
+++ gtmoviestore/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
+    path('accounts/', include('accounts.urls')),
     path("movies/", include("movies.urls")),
 ]
 
