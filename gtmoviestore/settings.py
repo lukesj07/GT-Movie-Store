a60030d4a6e238fe9abbace3294b356c1554749f
lukesj07 <lukesjennings07@gmail.com>
2025-02-10 19:15:20 -0500

    make base cart app

--- gtmoviestore/settings.py
+++ gtmoviestore/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "home",
     "accounts",
     "movies",
+    "cart",
 ]
 
 MIDDLEWARE = [
--- gtmoviestore/urls.py
+++ gtmoviestore/urls.py
@@ -23,7 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
-    path('accounts/', include('accounts.urls')),
+    path("accounts/", include("accounts.urls")),
     path("movies/", include("movies.urls")),
+    path("cart/", include("cart.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
