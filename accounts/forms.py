83b377095784e80c606f3095ec4a8010d0a14b6d
lukesj07 <lukesjennings07@gmail.com>
2025-02-13 17:18:12 -0500

    bugfixes and password reset added

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.forms  import UserCreationForm
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class CustomErrorList(ErrorList):
     def __str__(self):
@@ -18,3 +21,32 @@ class CustomUserCreationForm(UserCreationForm):
             self.fields[fieldname].widget.attrs.update(
                 {'class': 'form_control'}
             )
+
+class ResetPasswordForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    new_password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(ResetPasswordForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username', 'new_password', 'confirm_password']:
+            self.fields[fieldname].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        # Validate that user exists
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise ValidationError('Username does not exist.')
+
+        # Validate password match
+        if new_password and confirm_password:
+            if new_password != confirm_password:
+                raise ValidationError('Passwords do not match.')
